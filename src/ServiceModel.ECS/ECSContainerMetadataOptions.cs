cea06bd45c28154f7ced5db40f4621397e479db3
Renato Golia <renato.golia@educations.com>
2019-11-28 17:14:59 +0100

    Add ECS extension for ServiceModel

---
+++ src/ServiceModel.ECS/ECSContainerMetadataOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EMG.Extensions.Configuration.Model;
+
+namespace EMG.Utilities
+{
+    public class ECSContainerMetadataOptions
+    {
+        public Func<IReadOnlyList<PortMapping>, PortMapping> PortMappingSelector { get; set; } = list => list.FirstOrDefault();
+    }
+}
