891d464301b5bf8dba46bcda38903e282f5a620a
Renato Golia <renato.golia@educations.com>
2019-12-13 11:52:39 +0100

    Improved ServiceModel.ECS

--- src/ServiceModel.ECS/ECSMetadataExtensions.cs
+++ src/ServiceModel.ECS/ECSMetadataExtensions.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.ServiceModel.Discovery;
 using EMG.Extensions.Configuration.Model;
 using EMG.Utilities.ServiceModel.Configuration;
+using EMG.Utilities.ServiceModel.Discovery;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EMG.Utilities
 {
     public static class ECSMetadataExtensions
     {
+        [Obsolete("Use AddECSContainerSupport instead.")]
         public static HttpEndpointAddress UseECS(this HttpEndpointAddress endpointAddress, IConfiguration configuration, Action<ECSContainerMetadataOptions> configureOptions = null)
         {
             if (Environment.GetEnvironmentVariable(EMG.Extensions.Configuration.ECSMetadataExtensions.ECSContainerMetadataFileKey) == null)
@@ -34,6 +41,7 @@ namespace EMG.Utilities
             return EndpointAddress.ForHttp(containerMetadata.HostPrivateIPv4Address, endpointAddress.Path, portMapping.HostPort, endpointAddress.IsSecure);
         }
 
+        [Obsolete("Use AddECSContainerSupport instead.")]
         public static NetTcpEndpointAddress UseECS(this NetTcpEndpointAddress endpointAddress, IConfiguration configuration, Action<ECSContainerMetadataOptions> configureOptions = null)
         {
             if (Environment.GetEnvironmentVariable(EMG.Extensions.Configuration.ECSMetadataExtensions.ECSContainerMetadataFileKey) == null)
@@ -60,6 +68,59 @@ namespace EMG.Utilities
 
             return EndpointAddress.ForNetTcp(portMapping.HostPort, containerMetadata.HostPrivateIPv4Address, endpointAddress.Path);
 
+        }
+
+        public static IServiceCollection AddECSContainerSupport(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (Environment.GetEnvironmentVariable(EMG.Extensions.Configuration.ECSMetadataExtensions.ECSContainerMetadataFileKey) != null)
+            {
+                services.Configure<AnnouncementServiceOptions>(o => o.EndpointDiscoveryMetadata = GetMetadataForEndpoint);
+            }
+
+            return services;
+
+            EndpointDiscoveryMetadata GetMetadataForEndpoint(ServiceEndpoint endpoint)
+            {
+                var containerMetadata = configuration.Get<ECSContainerMetadata>();
+
+                var endpointMetadata = EndpointDiscoveryMetadata.FromServiceEndpoint(endpoint);
+
+                if (endpointMetadata is null)
+                {
+                    return null;
+                }
+
+                if (containerMetadata?.HostPrivateIPv4Address is null)
+                {
+                    return endpointMetadata;
+                }
+
+                var uriBuilder = new UriBuilder(endpointMetadata.Address.Uri)
+                {
+                    Host = containerMetadata.HostPrivateIPv4Address,
+                    Port = GetMappedPortOrDefault(containerMetadata.PortMappings, endpointMetadata.Address.Uri.Port)
+                };
+
+                var newMetadata = new EndpointDiscoveryMetadata
+                {
+                    Address = new System.ServiceModel.EndpointAddress(uriBuilder.Uri.ToString()),
+                    Version = endpointMetadata.Version
+                };
+
+                return newMetadata;
+            }
+
+            int GetMappedPortOrDefault(IEnumerable<PortMapping> mappings, int port)
+            {
+                var hostPortByContainerPort = mappings.ToDictionary(k => k.ContainerPort, v => v.HostPort);
+
+                if (hostPortByContainerPort.TryGetValue(port, out var mappedPort))
+                {
+                    return mappedPort;
+                }
+
+                return port;
+            }
         }
     }
 }
