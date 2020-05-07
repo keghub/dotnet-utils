97707ef521f24cb5e608ebfa8b5249661d900878
Renato Golia <renato.golia@outlook.com>
2020-05-07 14:57:58 +0200

    Imported DiscoveryAdapter into ServiceModel.DiscoveryAdapter

---
+++ src/ServiceModel/ServiceModel.DiscoveryAdapter/IDiscoveryAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+using System.Xml;
+
+namespace EMG.Utilities
+{
+    [ServiceContract]
+    public interface IDiscoveryAdapter
+    {
+        [OperationContract]
+        Uri Discover(XmlQualifiedName type);
+    }
+}
